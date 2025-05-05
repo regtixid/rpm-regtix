7ca30b17090dafea605642aec3e10a08c35ad36c
Mudana-Grune <mudanakomang@hotmail.com>
2025-05-06 00:25:55 +0800

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -25,6 +25,7 @@ class DatabaseSeeder extends Seeder
             SpecialPriceCSVSeeder::class,
             RegulerCSVSeeder::class
             // Add other seeders here
+            // Valid
         ]);
     }
 }
