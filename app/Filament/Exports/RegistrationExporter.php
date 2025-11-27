c59df8dea1b3822522c3f6b9f2f95eaf767c9aa0
balisnap-trip <regtixid@gmail.com>
2025-11-27 21:37:25 +0800

    Update RegistrationExporter.php

--- app/Filament/Exports/RegistrationExporter.php
+++ app/Filament/Exports/RegistrationExporter.php
@@ -7,18 +7,16 @@ use Filament\Actions\Exports\Enums\ExportFormat;
 use Filament\Actions\Exports\ExportColumn;
 use Filament\Actions\Exports\Exporter;
 use Filament\Actions\Exports\Models\Export;
-use Illuminate\Support\Facades\Log;
 
 class RegistrationExporter extends Exporter
 {
     protected static ?string $model = Registration::class;
-    
+
     public static function getColumns(): array
     {
         return [
             ExportColumn::make('id')
                 ->label('ID'),
-            ExportColumn::make('registration_code')->label('Kode Registrasi'),
             ExportColumn::make('ticketType.name')->label('Tiket'),
             ExportColumn::make('full_name')->label('Nama'),
             ExportColumn::make('email')->label('Email'),
@@ -41,12 +39,11 @@ class RegistrationExporter extends Exporter
             ExportColumn::make('bib_name')->label('Nama BIB'),
             ExportColumn::make('reg_id')->label('Nomer Registrasi'),
             ExportColumn::make('registration_date')->label('Tanggal Registrasi'),
-            ExportColumn::make('voucherCode.code')->label('Kode Voucher'),
             ExportColumn::make('gross_amount')->label('Gross Amount')
             ->formatStateUsing(function(Registration $record){
                 return $record->voucherCode?->voucher?->final_price ?? $record->categoryTicketType?->price ?? 0;
             }),
-            ExportColumn::make('invitation_code')->label('Kode Undangan'),
+            ExportColumn::make('invitation_code')->label('Kode Undangan'),            
         ];
     }
 
